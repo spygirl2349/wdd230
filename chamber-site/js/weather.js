9e8c8eb43515c81bff4f7372b38218f851c32aa2
Annabelle Ruiz <spygirl2349@gmail.com>
2021-12-14 17:17:11 -0700

    home page p1

---
+++ chamber-site/js/weather.js
@@ -0,0 +1,72 @@
+const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5605242&appid=ae3df22411a81915e415e33bf24600fd&units=imperial"
+
+const mydate = new Date ();
+const todayNum = mydate.getDay();
+
+const myweekday = new Array(7);
+myweekday[0] = "Sunday";
+myweekday[1] = "Monday";
+myweekday[2] = "Tuesday";
+myweekday[3] = "Wednesday";
+myweekday[4] = "Thursday";
+myweekday[5] = "Friday";
+myweekday[6] = "Saturday";
+
+let forecastDayNum =todayNum;
+
+fetch (apiURL)
+    .then((response) => response.json ())
+    .then((jsW) => {
+        console.log (jsW);
+        //CURRENT WEATHER GOES HERE
+        let current = document.createElement('p');
+        let temp = document.createElement('p');
+        let speed = document.createElement('p');
+        let humidity = document.createElement('p');
+
+        let cW = document.getElementById("currentW")
+
+        current.textContent = `Currently: ${jsW.list[0].weather[0].description}`;
+        temp.textContent = `Temp: ${jsW.list[0].main.temp}\xB0F`;
+        speed.textContent - `Wind Speed: ${jsW.list[0].wind.speed}`;
+        humidity.textContent = `Humidity: ${jsW.list[0].main.humidity}`;
+
+        cW.appendChild(current);
+        cW.appendChild(temp);
+        cW.appendChild(speed);
+        cW.appendChild(humidity);
+
+        //THREE DAY FORECAST
+        const mylist = jsW.list
+
+        for (i=0; i < mylist.length; i++) {
+        var time = mylist[i].dt_txt;
+        if (time.includes('18:00:00')) {
+            let theDayName = document.createElement('h5');
+            let theTemp = document.createElement("p");
+    
+            //Create icon and set src
+            const iconcode = mylist[i].weather[0].icon;
+            const icon_path = `//openweathermap.org/img/w/${iconcode}.png`;
+            let theIcon = document.createElement("img");
+            theIcon.src = icon_path;
+    
+            theDayName.textContent = myweekday[forecastDayNum];
+            theTemp.textContent = `${Math.round(jsW.list[i].main.temp)}\xB0F`;
+    
+            let theDay = document.createElement("div");
+            theDay.appendChild(theDayName);
+            theDay.appendChild(theTemp);
+            theDay.appendChild(theIcon);
+    
+            document.querySelector(".three-day").appendChild(theDay);
+    
+            forecastDayNum += 1;
+            if (forecastDayNum === 7){
+            forecastDayNum = 0;
+            }
+        }
+        }
+
+        
+    })
